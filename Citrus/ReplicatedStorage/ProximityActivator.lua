181f262952acff6c839fa804b336d58010ec31ca
Chaampus <79322508+Chaampus@users.noreply.github.com>
2022-04-09 18:22:58 -0500

    Update ProximityActivator.lua

--- Citrus/ReplicatedStorage/ProximityActivator.lua
+++ Citrus/ReplicatedStorage/ProximityActivator.lua
@@ -4,7 +4,7 @@
 -- ProximityActivator Class
 --
 --
--- # April 9 2022
+-- # April 5 2022
 -- / 
 --
 -- Written by Champus
@@ -39,21 +39,30 @@ function proximity:init()
 	Display.Name = 'Display'
 	Display.AlwaysOnTop = true
 	
+	local _Bg = Instance.new('Frame', Display)
+	_Bg.Size = UDim2.new(1,0,1,0)
+	_Bg.BackgroundTransparency = .2
+	_Bg.BackgroundColor3 = Color3.new(0,0,0)
+	_Bg.Name = 'ZIndex'
+	_Bg.BorderSizePixel = 0
+	_Bg.ClipsDescendants = true
+	
+	local Bg = Instance.new('Frame', _Bg)
+	Bg.Size = UDim2.new(0,0,1,0)
+	Bg.BackgroundTransparency = .8
+	Bg.Name = 'Background'
+	
 	local Title = Instance.new('TextLabel', Display)
 	Title.Size = UDim2.new(1,0,1,0)
-	Title.BackgroundTransparency = .6
+	Title.BackgroundTransparency = 1
 	Title.BackgroundColor3 = Color3.new(0,0,0)
 	Title.TextColor3 = Color3.fromRGB(255, 255, 255)
 	Title.TextScaled = true
 	Title.Font = Enum.Font.Gotham
 	Title.Text = tostring(self.Key):split('.')[3]
 	
-	local Bg = Instance.new('Frame', Title)
-	Bg.Size = UDim2.new(0,0,1,0)
-	Bg.BackgroundTransparency = .7
-	
 	local _UICorner = Instance.new('UICorner', Title)
-	local _UICorner = Instance.new('UICorner', Bg)
+	local _UICorner2 = Instance.new('UICorner', _Bg)
 	--
 	local Active = Instance.new("BoolValue", self.Location)
 	Active.Value = true
@@ -71,9 +80,6 @@ function proximity:init()
 	Range.Value = self.Range
 	Range.Name = 'Range'
 	
-	local Progress = Instance.new('NumberValue', self.Location)
-	Progress.Name = 'Progress'
-	
 	local Time = Instance.new('NumberValue', self.Location)
 	Time.Value = self.Time
 	Time.Name = 'Time'
@@ -86,9 +92,27 @@ function proximity:init()
 	--
 	
 	if RunService:IsClient() then
+		local Progress = Instance.new('NumberValue', self.Location)
+		Progress.Name = 'Progress'
+		
 		local x = Instance.new('BindableEvent', self.Location)
 		x.Event:Connect(self.Fire)
 	else
+		local function PlayerConnectedAsync(x)
+			local _y = self.Location:FindFirstChild(x.Name)
+			
+			if _y then
+				return _y
+			else
+				local _f = Instance.new('Folder', self.Location); _f.Name = x.Name
+				
+				local Progress = Instance.new('NumberValue', _f)
+				Progress.Name = 'Progress'
+				
+				return _f
+			end
+		end
+		
 		local x = Instance.new('RemoteEvent', self.Location); x.Name = 'ServerEvent'
 		x.OnServerEvent:Connect(function(...)
 			local c = select(1, ...).Character; if c then
@@ -102,7 +126,12 @@ function proximity:init()
 		
 		local Receive = Instance.new('RemoteFunction', self.Location); Receive.Name = 'Get'
 		Receive.OnServerInvoke = function(c, x)
-			return self.Location.Progress.Value
+			if x then
+				return PlayerConnectedAsync(c).Progress.Value
+			else
+				return (self.Time == 0)
+			end
+		
 		end
 
 		local Set = Instance.new('RemoteEvent', self.Location); Set.Name = 'Set'
@@ -110,11 +139,11 @@ function proximity:init()
 			c = c.Character; if x then
 				if (c:FindFirstChild('HumanoidRootPart')) and self.Location then
 					if (c.HumanoidRootPart.Position - self.Location.Position).Magnitude <= self.Range then
-						TweenService:Create(self.Location.Progress, TweenInfo.new(self.Location.Time.Value, Enum.EasingStyle.Cubic), {Value = self.Location.Time.Value}):Play()
+						TweenService:Create(PlayerConnectedAsync(c).Progress, TweenInfo.new(self.Location.Time.Value, Enum.EasingStyle.Linear), {Value = self.Location.Time.Value}):Play()
 					end
 				end
 			else
-				TweenService:Create(self.Location.Progress, TweenInfo.new(1/60, Enum.EasingStyle.Cubic), {Value = 0}):Play()
+				TweenService:Create(PlayerConnectedAsync(c).Progress, TweenInfo.new(1/60, Enum.EasingStyle.Cubic), {Value = 0}):Play()
 			end
 		end)
 	end
@@ -123,7 +152,7 @@ function proximity:init()
 	--
 	-- ... / Client Hook
 	
-	CollectionService:AddTag(self.Location, 'Proximity')
+	CollectionService:AddTag(self.Location, '__Proximity')
 end
 
 function proximity:IsA(str)
@@ -139,7 +168,7 @@ end
 -- # constructor "ProximityActivator"
 function proximity:new(location:Instance?, range:number?, time:number?, keycode:Enum.KeyCode, activation:()->()):Proximity
 	range = range or 15
-	time = time or 0.05
+	time = math.clamp(time, 0, 100) or 0
 	
 	local prox = setmetatable({}, self) 
 	prox.Location = location
